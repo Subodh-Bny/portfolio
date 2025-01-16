f8e95b005e2323612ecd5b7d406af8f4eedb52b8
Subodh-Bny <adhikarisubodh77@gmail.com>
2025-01-16 16:09:48 +0545

    change pic

--- src/app/(smooth-scroll)/about/about.tsx
+++ src/app/(smooth-scroll)/about/about.tsx
@@ -14,7 +14,7 @@ const About = () => {
         className="w-full flex justify-center mb-8"
       >
         <Image
-          src="/subodh-work.jpg"
+          src="/subodh-work.png"
           alt="subodh"
           height={300}
           width={300}
